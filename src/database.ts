ed76f4b62acfdaa2f0e5548c38f1ae8111379fbf
Huan LI (李卓桓) <zixia@zixia.net>
2020-07-05 01:21:16 +0800

    fix chatops & token room

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,7 @@
-const CHATOPS_ROOM_ID   = '18131996049@chatroom'  // 'ChatOps - BOT5 Wechaty'
+const CHATOPS_ROOM_ID   = '20533169841@chatroom' // ChatOps - Juzi.BOT
 const HEARTBEAT_ROOM_ID = '17376996519@chatroom'  // 'ChatOps - Heartbeat'
 
-const TOKEN_ROOM_ID = '18844011263@chatroom'
+const TOKEN_ROOM_ID = '9223372041354269363@im.chatroom' // Puppet Service Provider (TOKEN)
 
 export {
   CHATOPS_ROOM_ID,
